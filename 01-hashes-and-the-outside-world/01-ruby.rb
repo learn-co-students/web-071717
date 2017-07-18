469af53ca610859da2310386905ddbfc41dd3bbf
alexgriff <alexalexgriffith@gmail.com>
2017-07-18 10:57:40 -0400

    07/18 add folder

---
+++ 01-hashes-and-the-outside-world/01-ruby.rb
@@ -0,0 +1,10 @@
+def say_hi(name)
+  puts "hello #{name}"
+end
+
+['lindsey', 'johann']
+
+[{name: 'lindsey'}, {name: 'johann'}]
+
+
+reddit = "{"kind": "Listing", "data": {"modhash": "", "children": [{"kind": "t3", "data": {"contest_mode": false, "approved_at_utc": null, "banned_by": null, "media_embed": {}, "thumbnail_width": 140, "subreddit": "funny", "selftext_html": null, "selftext": "", "likes": null, "suggested_sort": null, "user_reports": [], "secure_media": null, "link_flair_text": null, "id": "6o003l", "banned_at_utc": null, "view_count": null, "secure_media_embed": {}, "clicked": false, "report_re"
