acae1abe7e6547b60bfa0c4923804ebde5d610f3
meryldakin <meryl.k.dakin@gmail.com>
2017-07-27 17:46:49 -0400

    added classes

--- oo-review/config/config/environment.rb
+++
@@ -1,9 +0,0 @@
-require 'bundler'
-Bundler.require
-
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/blog.db"
-)
-
-require_all 'app'
--- oo-review/config/environment.rb
+++ oo-review/config/environment.rb
@@ -2,7 +2,7 @@ require 'bundler'
 Bundler.require(:default)
 
 
-require_relative '../lib/animal'
+require_relative '../lib/zoo'
 
 
 Pry.start
