477c9716b13e9c01f5ba5808663f97b84dde264f
Johann Kerr <johannkerr@gmail.com>
2017-07-28 14:23:16 -0400

    adding doctor

--- 03-activerecord/hospital/app/models/patient.rb
+++ 03-activerecord/hospital/app/models/patient.rb
@@ -1,2 +1,19 @@
 class Patient < ActiveRecord::Base
 end
+
+
+class Cat
+
+  @@all = []
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    binding.pry
+  end
+
+  def self.all
+    @@all
+  end
+
+end
