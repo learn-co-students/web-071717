ff09426c1a7f3b82ef62955c5c0c9ea1391b56cc
Tim Campbell <timothylevicampbell@gmail.com>
2017-09-06 19:50:02 -0400

    Add render method to individual pokemon and refactor rendering logic for pokemon list

--- 10-pokemon-review/src/components/pokemon.js
+++ 10-pokemon-review/src/components/pokemon.js
@@ -8,4 +8,35 @@ class Pokemon {
   flipImage() {
 
   }
+
+  render() {
+    // function renderPokemon(pokemon) {
+    //
+    //   return `
+    //     <div class="pokemon-card" id="${pokemon.name}">
+    //       <div style="width:230px;margin:10px;background:#fecd2f;color:#2d72fc" class="pokemon-frame">
+    //       <h1 class="center-text">${pokemon.name}</h1>
+    //       <div style="width:239px;margin:auto">
+    //         <div style="width:96px;margin:auto">
+    //           <img id="${pokemon.name}-img" src="${pokemon.sprites[sprite] }">
+    //         </div>
+    //       </div>
+    //       <p style="padding:10px;" class="center-text flip-image" data-pokename="${pokemon.name}" data-action="flip-image">flip card</p>
+    //       </div>
+    //     </div>`
+    // }
+
+    return `
+      <div class="pokemon-card" id="${this.name}">
+        <div style="width:230px;margin:10px;background:#fecd2f;color:#2d72fc" class="pokemon-frame">
+        <h1 class="center-text">${this.name}</h1>
+        <div style="width:239px;margin:auto">
+          <div style="width:96px;margin:auto">
+            <img id="${this.name}-img" src="${this.frontImage}">
+          </div>
+        </div>
+        <p style="padding:10px;" class="center-text flip-image" data-pokename="${this.name}" data-action="flip-image">flip card</p>
+        </div>
+      </div>`
+  }
 }
