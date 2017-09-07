cbec75aa1ab10db977939bf4539f17a1016d7154
Tim Campbell <timothylevicampbell@gmail.com>
2017-09-06 20:04:07 -0400

    Remove unused html and css

--- 10-pokemon-review/src/components/pokemon.js
+++ 10-pokemon-review/src/components/pokemon.js
@@ -17,14 +17,15 @@ class Pokemon {
   render() {
     return `
       <div class="pokemon-card" id="${this.name}">
-        <div style="width:230px;margin:10px;background:#fecd2f;color:#2d72fc" class="pokemon-frame">
-        <h1 class="center-text">${this.name}</h1>
-        <div style="width:239px;margin:auto">
-          <div style="width:96px;margin:auto">
-            <img id="${this.name}-img" src="${this.currentImage}">
+        <div style="width:230px;margin:10px;background:#fecd2f;color:#2d72fc">
+          <h1 class="center-text">${this.name}</h1>
+          <div style="width:239px;margin:auto">
+            <div style="width:96px;margin:auto">
+              <img src="${this.currentImage}">
+            </div>
           </div>
-        </div>
-        <p style="padding:10px;" class="center-text flip-image" data-pokename="${this.name}" data-action="flip-image">flip card</p>
+
+          <p style="padding:10px;" class="center-text flip-image" data-pokename="${this.name}">flip card</p>
         </div>
       </div>`
   }
